5749deb6223cab13c8f49c6bb49cf1a6ac54a509
REDDEDD <pablo.diaz.92@outlook.com>
2018-05-23 00:33:26 +0200

    first commit
    ofertometro web index

---
+++ index.php
@@ -0,0 +1,56 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
+		<link rel="stylesheet/less" type="text/css" href="css/estilos.less" />
+		<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.0.2/less.min.js" ></script>
+		<title>Ofertometro</title>
+	</head>
+	<body>
+		<!-- CONTENEDOR GLOBAL -->
+		<div class="container-fluid sin-margen col-md-12">
+			<div class="col-md-12 navbar-light bg-light position-fixed">
+				<nav class="navbar navbar-expand-lg auto" style="padding-bottom:0px!important;padding-top:0px!important">
+					<a class="navbar-brand" href="#" style="width:20%"><h2 style="padding-top:4%"><img style="width:20%;height:100%" src="img/termometro.png">Ofertometro</h2></a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				    	<span class="navbar-toggler-icon"></span>
+					</button>
+
+				  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+				    <ul class="navbar-nav">
+				      <li class="nav-item dropdown">
+				        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				          Ofertas
+				        </a>
+				        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				          <a class="dropdown-item" href="#">Todo</a>
+				          <a class="dropdown-item" href="#">Mejores ofertas</a>  
+				          <a class="dropdown-item" href="#">Nuevos</a>
+				        </div>
+				      </li>
+				    </ul>
+				    <form class="form-inline my-2 my-lg-0 mr-auto">
+				      <input style="border-radius: 8px 0px 0px 8px;" class="form-control mr-sm-2" type="search" placeholder="..." aria-label="Search">
+				      <button style="border-radius: 0px 8px 8px 0px;margin-left:-8px" class="btn btn-outline-info my-2 my-sm-0" type="submit">Buscar</button>
+				    </form>
+				    <ul class="navbar-nav margin-left-auto">
+				    	<li class="nav-item" style="margin-top:2%">
+					    	<a class="nav-link" href="#">Quienes somos</a>
+					    </li>
+					    <li class="nav-item">
+					    	<a class="nav-link" href="#"><button type="button" class="btn btn-dark">Registro</button></a>
+					    </li>
+					    <li class="nav-item">
+					    	<a class="nav-link" href="#"><button type="button" class="btn btn-dark" style="margin-left:-10%">Log in</button></a>
+					    </li>
+					</ul>
+				  </div>
+				</nav>
+			</div>
+		</div>
+	</body>
+</html>
